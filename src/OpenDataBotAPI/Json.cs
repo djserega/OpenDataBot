3e9194d9b5aaf8c805b1db079214f4f2d03fcf61
dj_serega <galyuk.sergey@gmail.com>
2018-03-23 09:00:43 +0200

    deserialize T, List<T>

--- src/OpenDataBotAPI/Json.cs
+++ src/OpenDataBotAPI/Json.cs
@@ -9,7 +9,12 @@ namespace OpenDataBotAPI
 {
     internal static class Json<T> where T : class, new()
     {
-        internal static List<T> DeserializeString(string input)
+        internal static T DeserializeString(string input)
+        {
+            return new JavaScriptSerializer().Deserialize<T>(input);
+        }
+
+        internal static List<T> DeserializeStringToList(string input)
         {
             return new JavaScriptSerializer().Deserialize<List<T>>(input);
         }
